f0fc1fe83891ba9dd3970887c3469ce13d6669f0
Your Name <roihansori12@gmail.com>
2025-12-17 01:16:30 +0700

    Feat: add api for search item

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,20 +1,27 @@
+using SearchService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error during database initialization: {ex.Message}");
+
+}
 
 app.Run();
