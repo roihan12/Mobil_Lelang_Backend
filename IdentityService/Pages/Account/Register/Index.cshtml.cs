49189d60576ede07f92734394db69e9d8278215a
Your Name <roihansori12@gmail.com>
2025-12-20 12:48:01 +0700

    Feat: Add Identity service for authentication

---
+++ IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,71 @@
+using Duende.IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace IdentityService.Pages.Account.Register
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class Index : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public Index(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+
+        [BindProperty]
+        public bool RegisterSuccessful { get; set; }
+
+
+        public IActionResult OnGet(string returnUrl)
+        {
+            Input = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (Input.Button != "register")
+            {
+                return Redirect("~/");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    EmailConfirmed = true
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimsAsync(user, new Claim[]{
+                                new Claim(JwtClaimTypes.Name, Input.FullName),
+                            });
+                    RegisterSuccessful = true;
+                    return RedirectToPage("/Account/Login");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return Page();
+        }
+    }
+}
